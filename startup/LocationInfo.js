d895bc7a9c7db3f81a0dfad9b2f3db19de132856
kelecant <Kelecant@gmail.com>
2024-02-28 16:12:45 -0700

    Added Java files and Login Function

---
+++ startup/LocationInfo.js
@@ -0,0 +1,18 @@
+const nameEl = document.querySelector("#name");
+localStorage.setItem("userName", nameEl.value);
+
+//Items I need to store
+//LocationName
+//Review 1
+//Recomendation
+//Comments
+
+
+
+//Total overview
+// 1. Store Username from home page
+// 2. create Location class that stores data
+// 3. make the add button store data
+// 4. modify generic page to display data from location class
+// 5. place a chatbox in page
+// 6. modify locations list to be dependant on locations data
