52a04cbc399f62ebce849a382248a5bc4cd7e31b
kelecant <Kelecant@gmail.com>
2024-02-28 21:12:55 -0700

    Created createLocation function

--- startup/LocationInfo.js
+++ startup/LocationInfo.js
@@ -10,9 +10,19 @@ localStorage.setItem("userName", nameEl.value);
 
 
 //Total overview
-// 1. Store Username from home page
 // 2. create Location class that stores data
 // 3. make the add button store data
 // 4. modify generic page to display data from location class
 // 5. place a chatbox in page
 // 6. modify locations list to be dependant on locations data
+
+
+function createLocation (){
+    const adress = document.querySelector("#adress");
+    localStorage.setItem("Adress", adress.value);
+    const review = document.querySelector("#review");
+    localStorage.setItem("Review", review.value);
+    const again = document.querySelector("#again");
+    localStorage.setItem("again", username.value);
+
+}
