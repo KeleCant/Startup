0bc7ea17f5f67aa44ea00b478cacb534dc2fdb74
Kele Kent <94769317+KeleCant@users.noreply.github.com>
2024-04-11 21:39:39 -0600

    working on index

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -3,5 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+      <div className='body bg-dark text-light'>
+        <body>
+            <header>
+                <div class="inline"> <a href="index.html">Home</a> </div>
+                <div class="inline"> <a href="locationlist.html">Locations</a> </div>
+                <div class="inline"> <a href="addlocations.html">Add Location</a> </div>
+                <div class="inline"> <a href="about.html">About</a></div>
+            </header>
+  
+            <main>App components go here</main>
+  
+            <footer>
+                <a href="https://github.com/KeleCant/Startup">GitHub</a>
+            </footer>
+        </body>
+      </div>
+    );
+  }
