eddf39252e11534ba4bada43ffa0a3faaa9e1231
Kele Kent <94769317+KeleCant@users.noreply.github.com>
2024-04-12 03:17:34 -0600

    new file working

--- startup/src/locationlist/locationlist.jsx
+++ startup/src/locationlist/locationlist.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Locationlist() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div> locationlist displayed here </div>
+    <main>
+      <h1> What To Do: Provo </h1>
+      <h1> Places to visit </h1>
+      <div className="locations">
+        <div className="new location"> <NavLink className='nav-link' to='/locationinfo'> The Taste </NavLink> </div>
+        <div className="new location"> <NavLink className='nav-link' to='/locationinfo'> The Vibe </NavLink> </div>
+        <div className="new location"> <NavLink className='nav-link' to='/locationinfo'> Dutch Bros </NavLink> </div>
+      </div>
     </main>
   );
 }
+
+export default Locationlist;
